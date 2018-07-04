61cc4358ee777b9968df16d57e614c12b3a42826
Hutaimu1 <10-28-08@sjtu.edu.cn>
2018-07-04 17:19:02 +0800

    Add files via upload

---
+++ main/login.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Form, Icon, Input, Button} from 'antd';
+import {Link} from 'react-router-dom';
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
+    handleUserName = (rule, value, callback) => {
+        let reg = /^[A-Za-z]+$/;
+        if (value && !reg.test(value)) {
+            callback('用户名必须以字母开头!')
+        }
+        callback()
+    };
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{required: true, message: '用户名不能为空!'}, {
+                            validator: this.handleUserName
+                        }],
+                    })(
+                        <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="用户名"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{required: true, message: '密码不能为空!'}],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password"
+                               placeholder="密码"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" className="login-form-button"><Link to='/home'>
+                        登录</Link>
+                    </Button>
+                    <a href="">忘记密码?</a>
+                    <a className="login-form-register" href="">新用户注册!</a>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+export default WrappedNormalLoginForm;
