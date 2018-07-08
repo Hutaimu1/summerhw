46210615375f31fbb03ae00e758f5ac228ad56c2
longer150721 <275868482@qq.com>
2018-07-08 21:55:11 +0800

    票品查询界面部分实现

--- jupiaowang/src/BigComponrnts/trainTicket.js
+++ jupiaowang/src/BigComponrnts/trainTicket.js
@@ -6,11 +6,13 @@ import moment from 'moment';
 class trainTicket extends React.Component {
     state = {
         plainCheckedList: [],
-        timeCheckedList:[false,true,false,false],
-        starting:"上海",
-        destination:"北京",
-        date:moment(),
-        timeVisible: false,
+        startTimeCheckedList: [false, true, false, false],
+        arriveTimeCheckedList: [false, false, true, false],
+        starting: "上海",
+        destination: "北京",
+        date: moment(),
+        startTimeVisible: false,
+        arriveTimeVisible: false
     };
 
     handleSubmit = (e) => {
@@ -18,9 +20,9 @@ class trainTicket extends React.Component {
         let destination = this.props.form.getFieldValue('destination');
         let date = this.props.form.getFieldValue('date');
         this.setState({
-            starting:starting,
-            destination:destination,
-            date:date
+            starting: starting,
+            destination: destination,
+            date: date
         });
     };
 
@@ -77,42 +79,86 @@ class trainTicket extends React.Component {
         });
     };
 
-    onTimeChange = (e) => {
+    onStartTimeChange = (e) => {
         let index = e.target.index;
-        let timeCheckedList = this.state.timeCheckedList;
-        timeCheckedList[index] = !timeCheckedList[index];
+        let startTimeCheckedList = this.state.startTimeCheckedList;
+        startTimeCheckedList[index] = !startTimeCheckedList[index];
         this.setState({
-            timeCheckedList:timeCheckedList
+            startTimeCheckedList: startTimeCheckedList
         });
     };
 
-    handleTimeVisibleChange = (flag) => {
-        this.setState({ timeVisible: flag });
+    onArriveTimeChange = (e) => {
+        let index = e.target.index;
+        let arriveTimeCheckedList = this.state.arriveTimeCheckedList;
+        arriveTimeCheckedList[index] = !arriveTimeCheckedList[index];
+        this.setState({
+            arriveTimeCheckedList: arriveTimeCheckedList
+        });
     };
 
-    handleClose = (index) =>{
-        let timeCheckedList = this.state.timeCheckedList;
-        timeCheckedList[index] = !timeCheckedList[index];
+    handleStartTimeVisibleChange = (flag) => {
+        this.setState({startTimeVisible: flag});
+    };
+
+    handleArriveTimeVisibleChange = (flag) => {
+        this.setState({arriveTimeVisible: flag});
+    };
+
+    handleStartClose = (index) => {
+        let startTimeCheckedList = this.state.startTimeCheckedList;
+        startTimeCheckedList[index] = !startTimeCheckedList[index];
         this.setState({
-            timeCheckedList:timeCheckedList
+            startTimeCheckedList: startTimeCheckedList
         })
+    };
+
+    handleArriveClose = (index) => {
+        let arriveTimeCheckedList = this.state.arriveTimeCheckedList;
+        arriveTimeCheckedList[index] = !arriveTimeCheckedList[index];
+        this.setState({
+            arriveTimeCheckedList: arriveTimeCheckedList
+        })
+    };
+
+    isDisplay = () => {
+        if (this.state.startTimeCheckedList.indexOf(true) > -1 || this.state.arriveTimeCheckedList.indexOf(true) > -1){
+            return "";
+        }
+        return "none";
     };
 
     render() {
         const {getFieldDecorator} = this.props.form;
-        const plainOptions = ['高铁G/C', '动车D', '特快T','快速K', '直达Z', '其他'];
-        const timeOptions = ['凌晨（0:00 - 6:00）', '上午（6:00 - 12:00）','下午（12:00 - 18:00）','晚上（18:00 - 24:00）'];
-        const menu = (
+        const plainOptions = ['高铁G/C', '动车D', '特快T', '快速K', '直达Z', '其他'];
+        const timeOptions = ['凌晨（0:00 - 6:00）', '上午（6:00 - 12:00）', '下午（12:00 - 18:00）', '晚上（18:00 - 24:00）'];
+        const startMenu = (
             <Menu>
-                <Menu.Item><Checkbox index={0} checked={this.state.timeCheckedList[0]} onChange={this.onTimeChange}>{timeOptions[0]}</Checkbox></Menu.Item>
-                <Menu.Item><Checkbox index={1} checked={this.state.timeCheckedList[1]} onChange={this.onTimeChange}>{timeOptions[1]}</Checkbox></Menu.Item>
-                <Menu.Item><Checkbox index={2} checked={this.state.timeCheckedList[2]} onChange={this.onTimeChange}>{timeOptions[2]}</Checkbox></Menu.Item>
-                <Menu.Item><Checkbox index={3} checked={this.state.timeCheckedList[3]} onChange={this.onTimeChange}>{timeOptions[3]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={0} checked={this.state.startTimeCheckedList[0]}
+                                     onChange={this.onStartTimeChange}>{timeOptions[0]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={1} checked={this.state.startTimeCheckedList[1]}
+                                     onChange={this.onStartTimeChange}>{timeOptions[1]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={2} checked={this.state.startTimeCheckedList[2]}
+                                     onChange={this.onStartTimeChange}>{timeOptions[2]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={3} checked={this.state.startTimeCheckedList[3]}
+                                     onChange={this.onStartTimeChange}>{timeOptions[3]}</Checkbox></Menu.Item>
+            </Menu>
+        );
+        const arriveMenu = (
+            <Menu>
+                <Menu.Item><Checkbox index={0} checked={this.state.arriveTimeCheckedList[0]}
+                                     onChange={this.onArriveTimeChange}>{timeOptions[0]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={1} checked={this.state.arriveTimeCheckedList[1]}
+                                     onChange={this.onArriveTimeChange}>{timeOptions[1]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={2} checked={this.state.arriveTimeCheckedList[2]}
+                                     onChange={this.onArriveTimeChange}>{timeOptions[2]}</Checkbox></Menu.Item>
+                <Menu.Item><Checkbox index={3} checked={this.state.arriveTimeCheckedList[3]}
+                                     onChange={this.onArriveTimeChange}>{timeOptions[3]}</Checkbox></Menu.Item>
             </Menu>
         );
         return (
             // eslint-disable-next-line
-            <div style={{fontWeight:"bold"}}>
+            <div style={{fontWeight: "bold"}}>
                 <Form layout="inline" onSubmit={this.handleSubmit}>
                     <Form.Item
                         label="出发站"
@@ -154,32 +200,53 @@ class trainTicket extends React.Component {
                         </Button>
                     </Form.Item>
                 </Form>
-                <Divider orientation="left" style={{fontWeight:"bold"}}>选择</Divider>
-                <h3 style={{fontWeight:"bold",marginBottom:"20px"}}>
+                <Divider orientation="left" style={{fontWeight: "bold"}}>选择</Divider>
+                <h3 style={{fontWeight: "bold", marginBottom: "20px"}}>
                     <span> {this.state.starting} <em className="arrow-ico">&nbsp;</em> {this.state.destination}</span>
                     <span>（{this.state.date._d.getMonth() + 1}月{this.state.date._d.getDate()}日 {this.getWeek(this.state.date)})</span>
                 </h3>
-                <Checkbox.Group options={plainOptions} value={this.state.plainCheckedList} onChange={this.onPlainChange} />
-                <Divider orientation="left" style={{fontWeight:"bold"}}>筛选</Divider>
-                <Dropdown overlay={menu}
-                          onVisibleChange={this.handleTimeVisibleChange}
-                          visible={this.state.timeVisible}
+                <Checkbox.Group options={plainOptions} value={this.state.plainCheckedList}
+                                onChange={this.onPlainChange}/>
+                <Divider orientation="left" style={{fontWeight: "bold"}}>筛选</Divider>
+                <Dropdown overlay={startMenu}
+                          onVisibleChange={this.handleStartTimeVisibleChange}
+                          visible={this.state.startTimeVisible}
                 >
-                    <Button style={{fontWeight:"bold"}}>
-                        出发时段 <Icon type="down" />
+                    <Button style={{fontWeight: "bold"}}>
+                        出发时段 <Icon type="down"/>
                     </Button>
                 </Dropdown>
-                <Divider orientation="left" style={{fontWeight:"bold"}}>已选</Divider>
-                <div>{this.state.timeCheckedList.map((tag, index) => {
-                    if (tag === true){
-                        return (<Tag key ={index} closable afterClose={() => this.handleClose(index)} color="#108ee9">
+                <Dropdown overlay={arriveMenu}
+                          onVisibleChange={this.handleArriveTimeVisibleChange}
+                          visible={this.state.arriveTimeVisible}
+                >
+                    <Button style={{fontWeight: "bold",marginLeft:"10px"}}>
+                        出发时段 <Icon type="down"/>
+                    </Button>
+                </Dropdown>
+                <Divider orientation="left" style={{fontWeight: "bold",display:this.isDisplay()}} >已选</Divider>
+                <div>{this.state.startTimeCheckedList.map((tag, index) => {
+                    if (tag === true) {
+                        return (<Tag key={index} closable afterClose={() => this.handleStartClose(index)} color="#108ee9">
                             出发于{timeOptions[index]}
                         </Tag>);
                     }
                     else {
                         return null
                     }
-                })}</div>
+                })}
+                    {this.state.arriveTimeCheckedList.map((tag, index) => {
+                        if (tag === true) {
+                            return (
+                                <Tag key={index} closable afterClose={() => this.handleArriveClose(index)} color="#108ee9">
+                                    到达于{timeOptions[index]}
+                                </Tag>);
+                        }
+                        else {
+                            return null
+                        }
+                    })}
+                </div>
             </div>
         );
     }
